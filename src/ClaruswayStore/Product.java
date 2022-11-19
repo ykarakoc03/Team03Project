f57112f8580fb87690fdde9fee917bdfd45a0bfa
ykarakoc03 <delisuvarim@gmail.com>
2022-11-19 23:29:09 +0300

    ProductAbstract_Class_Create

---
+++ src/ClaruswayStore/Product.java
@@ -0,0 +1,10 @@
+package JavaProjects.ClaruswayStore;
+
+public abstract class Product {
+
+    abstract void menu();
+    abstract void addItem();
+    abstract void getProducts();
+    abstract void deleteItem();
+
+}
