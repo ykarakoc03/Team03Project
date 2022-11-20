cefd44e12ec4ae670809746c65e78914a94ab703
ykarakoc03 <delisuvarim@gmail.com>
2022-11-20 23:39:24 +0300

    ProductAbst_Class_Create

--- src/ClaruswayStore/Product.java
+++ src/ClaruswayStore/Product.java
@@ -1,10 +1,25 @@
 package JavaProjects.ClaruswayStore;
 
+import java.util.Scanner;
+
 public abstract class Product {
+    Scanner scanner = new Scanner(System.in);
+    int id;// Ürün id si
+    String name;//Ürün ismi
+    double price;//Ürün fiyatı
+    double discountRate;//Ürün indirim oranı
+    int amount;//Ürün stoğu
+    String brand;//Brand
+    double screenSize;// Ürün ekran boyutu
+    String ram;// Ürün ram i
+    String memory;// Ürün hafızası
 
     abstract void menu();
+
     abstract void addItem();
+
     abstract void getProducts();
+
     abstract void deleteItem();
 
 }
